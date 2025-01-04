2ed2297ae9a8e8ffe1caeb2134f528f7a68d1b76
gajj44 <gajjunaphade@gmail.com>
2025-01-05 01:16:56 +0530

    added installed framer and RecentWork changes

---
+++ src/Pages/HomePage/Work.js
@@ -0,0 +1,63 @@
+import Magnifyx from '../../assets/RecentWork/Magnify.png'
+import NowsBansi from '../../assets/RecentWork/Bansi.png'
+import FrenchNYou from '../../assets/RecentWork/FrenchNYou.png'
+import Rediance from '../../assets/RecentWork/Rediance.png'
+import MamtaPortfolio from '../../assets/RecentWork/MamtaPortfolio.png'
+import Deltasafe from '../../assets/RecentWork/Deltasafe.png'
+import Gtube from '../../assets/RecentWork/Gtube.png'
+import Elearners from '../../assets/RecentWork/Elearners.png'
+
+export const WorkData=[
+    {
+        title: 'Magnifyx',
+        img: Magnifyx,
+        desc: 'Magnifyx is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'Web Development',
+        category2: 'UI/UX',
+    },
+    {
+        title: 'Rediance',
+        img: Rediance,
+        desc: 'Rediance is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'Web Development',
+        category2: 'UI/UX',
+    },
+    {
+        title: 'Deltasafe',
+        img: Deltasafe,
+        desc: 'Deltasafe is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'Web Development',
+        category2: 'UI/UX',
+    },
+    {
+        title: 'FrenchNYou',
+        img: FrenchNYou,
+        desc: 'FrenchNYou is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'Web Development',
+    },
+    {
+        title: 'Gtube',
+        img: Gtube,
+        desc: 'Gtube is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'Web Development',
+    },
+    {
+        title: 'NowsBansi',
+        img: NowsBansi,
+        desc: 'NowsBansi is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'UI/UX',
+    },
+    {
+        title: 'MamtaPortfolio',
+        img: MamtaPortfolio,
+        desc: 'MamtaPortfolio is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'Web Development',
+    },
+    {
+        title: 'Elearners',
+        img: Elearners,
+        desc: 'Elearners is a platform that allows you to create and manage your own online store. It is a platform that allows you to create and manage your own online store.',
+        category: 'Web Development',
+        category2: 'UI/UX'
+    }
+]
