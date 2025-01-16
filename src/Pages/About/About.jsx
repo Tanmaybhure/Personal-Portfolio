e426de1b0e8ac315688d8e86bc831f2558b1fb43
gajj44 <gajjunaphade@gmail.com>
2025-01-17 00:44:54 +0530

    about us fix

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,6 +2,9 @@ import React,{useRef,useEffect,useState}  from 'react'
 import HeroOtherPages from '../../Components/SharedComponent/HeroOtherPages'
 import Navbar from '../../Components/SharedComponent/Navbar'
 import EAndEAbout from '../../Components/AboutComponents/EAndEAbout';
+import Skills from '../HomePage/Skills';
+import Stats from '../HomePage/Stats';
+import Footer from '../../Components/SharedComponent/Footer';
 function About() {
 
  const [isScrolled, setIsScrolled] = useState(false);
@@ -34,12 +37,24 @@ function About() {
 
   return (
    <>
-      <div  ref={scrollRef} className='flex flex-col items-center scrollDiv gap-10 lg:gap-20 w-full overflow-x-hidden h-screen font-sora will-change-auto scroll-smooth'>
+      <div  ref={scrollRef} className='flex bg-[#14091c] flex-col items-center scrollDiv gap-10 lg:gap-20 w-full overflow-x-hidden h-screen font-sora will-change-auto scroll-smooth'>
 
     <Navbar isScrolled={isScrolled} />
    <HeroOtherPages Title={'About'} Path={'Home'} Subpath={'About'}/>
     <EAndEAbout/>
+    <div className='w-full  relative -translate-y-72'>
+    <div className="blur-[150px] bg-[#8750f7] absolute !top-20 right-0 !w-[230px] !h-[300px]"/>
 
+    <Skills/>
+    </div>
+    
+    <div className='w-full bg-[#0e0714] h-fit -translate-y-72'>
+    <Stats/>
+    </div>
+
+    
+
+    <Footer/>
    </div>
 
    </>
