6f0516ad8447f4196ed50468b81881d3c6478ad9
gajj44 <gajjunaphade@gmail.com>
2025-01-18 15:03:23 +0530

    build fix

--- src/Data/SkillsData.js
+++ src/Data/SkillsData.js
@@ -1,6 +1,6 @@
 import ReactLogo from '../assets/Skills/react.svg'
 import JsLogo from '../assets/Skills/JS.svg'
-import CSSLogo from '../assets/Skills/Css.svg'
+import CSSLogo from '../assets/Skills/CSS.svg'
 import HtmlLogo from '../assets/Skills/Html.svg'
 import TailwindLogo from '../assets/Skills/Tailwind.svg'
 import FigmaLogo from '../assets/Skills/Figma.svg'
